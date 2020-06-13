5b774e5c0cc764f73aa9dfc2e8322871da866c5d
minhlv89 <vanminh89.le@gmail.com>
2020-06-13 10:41:06 -0700

    parsing code and unit test

--- tlm_parser.h
+++ tlm_parser.h
@@ -2,56 +2,77 @@
 #pragma comment(lib, "Ws2_32.lib")
 #include<iostream>
 #include <stdint.h>
-#ifdef _WIN32
+#ifdef OS_WINDOWS
 #include <WinSock2.h>
 #else
 #include <arpa/inet.h>
 #endif
 #include "tlm.h"
+#include "rx_interface.h"
 
 #define MAGIC_HEADER				0xABBACFFC
 #define MAGIC_HEADER_LENGTH			4
 #define PACKET_LENGTH				12
-#define PROTOCOL_BIT_MASK			0xE0
-#define SUBSYSTEM_BIT_MASK			0x1F
-#define COMPONENT_ID_BIT_MASK		0xF8
-#define TELEMETRY_TYPE_BIT_MASK		0x07
+#define PROTOCOL_BIT_MASK			0xE0	//1110 0000
+#define SUBSYSTEM_BIT_MASK			0x1F	//0001 1111
+#define COMPONENT_ID_BIT_MASK			0xF8	//1111 1000
+#define TELEMETRY_TYPE_BIT_MASK			0x07	//0000 0111
+
+#define SUBSYSTEM_AOCS		1
+#define SUBSYSTEM_CDH		3
+#define SUBSYSTEM_COM		5
+
+#define COMPONENT_CDH		0
+#define COMPONENT_COM_1		1
+#define COMPONENT_COM_2		2
+#define COMPONENT_COM_3		10
+#define COMPONENT_COM_4		20
 class Parser {
 public:
-	int detectPkt(int8_t byte) {
-		int8_t position = 1;
-		int8_t packetByte;
+	int detectPkt(uint8_t byte) {
+		//printf("Parser::detectPkt byte = %2x\n", byte);
+		uint8_t position = 4;
+		uint8_t packetByte;
+		/*
 		if (byte != 0xAB) {
 			return 0;
 		}
+		*/
+		if(0 == detectMagicHeader(byte)){
+			return 0;
+		}
 		else {
-			int8_t packetChecksum 0;
-			packetChecksum = packetChecksum ^ byte;
+			uint8_t packetChecksum = 0;
+			payload.tempIVal = 0;
 			for(int i = 1; i < PACKET_LENGTH; i++) {
-				packetByte = iface.getByte();
+				//packetByte = getByte();
+
+				packetByte = PacketInterface::getByte();
 				switch (position)
 				{
+				/*
 				case 1:
 					if (packetByte != 0xBA) {
+						printf("Parser::detectPkt Error magic 1\n");
 						return 0;
 					}
-					packetChecksum = packetChecksum ^ packetByte;
 					position++;
 					break;
 				case 2:
 					if (packetByte != 0xCF) {
+						printf("Parser::detectPkt Error magic 2\n");
 						return 0;
 					}
-					packetChecksum = packetChecksum ^ packetByte;
 					position++;
 					break;
 				case 3:
 					if (packetByte != 0xFC) {
+						printf("Parser::detectPkt Error magic 3\n");
 						return 0;
 					}
-					packetChecksum = packetChecksum ^ packetByte;
 					position++;
 					break;
+				*/
 				case 4:
 					secondaryHeader1 = packetByte;
 					packetChecksum = packetChecksum ^ packetByte;
@@ -67,29 +88,30 @@ public:
 					position++;
 					break;
 				case 7:
-					payload.tempIVal |= int32_t(packetByte) << 24;
+					payload.tempIVal |= uint32_t(packetByte) << 24;
 					packetChecksum = packetChecksum ^ packetByte;
 					position++;
 					break;
 				case 8:
-					payload.tempIVal |= int32_t(packetByte) << 16;
+					payload.tempIVal |= uint32_t(packetByte) << 16;
 					packetChecksum = packetChecksum ^ packetByte;
 					position++;
 					break;
 				case 9:
-					payload.tempIVal |= int32_t(packetByte) << 8;
+					payload.tempIVal |= uint32_t(packetByte) << 8;
 					packetChecksum = packetChecksum ^ packetByte;
 					position++;
 					break;
 				case 10:
-					payload.tempIVal |= int32_t(packetByte) << 0;
+					payload.tempIVal |= uint32_t(packetByte) << 0;
 					packetChecksum = packetChecksum ^ packetByte;
 					position++;
-					payload.tempIVal = ntohl(payload.tempIVal);
+					//printf("Parser::detectPkt tempIval:0x%x\n", payload.tempIVal);
 					break;
 				case 11:
 					checksum = packetByte;
 					if (checksum != packetChecksum) {
+						//printf("Parser::detectPkt Error checksum %2x, %2x\n", checksum, packetChecksum);
 						return 0;
 					}
 					position++;
@@ -99,22 +121,103 @@ public:
 				}
 			}
 		}
+		//printf("Parser::detectPkt Finish to parse packet\n");
+
 		return 1;
 	}
 	
 	int extractData(CompoTlm_t &tlm) {
-		protocol = secondaryHeader1 & PROTOCOL_BIT_MASK;
-		if (protocol != 1 || protocol != 2) {
+		protocol = (secondaryHeader1 & PROTOCOL_BIT_MASK) >>5;
+
+		if (protocol != 1 && protocol != 2) {
 			return 1;
 		}
-		tlm.subsys_id = secondaryHeader1 & SUBSYSTEM_BIT_MASK;
-		tlm.compo_id = secondaryHeader2 & COMPONENT_ID_BIT_MASK;
+		subsystemID = secondaryHeader1 & SUBSYSTEM_BIT_MASK;
+		componentID = (secondaryHeader2 & COMPONENT_ID_BIT_MASK) >>3;
+
+		if(subsystemID == SUBSYSTEM_AOCS){
+			if(0 == validateAocsComponentID(subsystemID)){
+				return 1;
+			}
+		}
+		else if(subsystemID == SUBSYSTEM_CDH){
+			if(componentID != COMPONENT_CDH){
+				return 1;
+			}
+		}
+		else if(subsystemID == SUBSYSTEM_COM){
+			if(componentID != COMPONENT_COM_1 && componentID != COMPONENT_COM_2 &&
+				componentID != COMPONENT_COM_3 && componentID != COMPONENT_COM_4){
+				return 1;
+			}
+		}
+		else{
+			return 1;
+		}
+		tlm.subsys_id = subsystemID;
+		tlm.compo_id = componentID;
 		tlmType = secondaryHeader2 & TELEMETRY_TYPE_BIT_MASK;
+
+
 		if (tlmType != 1) {
+			printf("Parser::extractData invalid Telemetry:%d\n",protocol);
 			return 1;
 		}
 		else {
 			tlm.temperature = payload.tempFVal;
+		}
+		//printf("Parser::extractData: tlmType:%d, subsys:%d, compo:%d, temp=%.1f\n"
+		//	,tlmType, tlm.subsys_id, tlm.compo_id, tlm.temperature);
+		return 0;
+	}
+private:
+	int detectMagicHeader(uint8_t byte){
+		if(byte != 0xAB){
+			return 0;
+		}
+		else{
+			while(PacketInterface::existRcvData()){
+				uint8_t packetByte = PacketInterface::getByte();
+				if(packetByte != 0xBA){
+					if(packetByte = 0xAB){
+						continue;
+					}
+					else{
+						return 0;
+					}
+				}
+				packetByte = PacketInterface::getByte();
+				if(packetByte != 0xCF){
+					if(packetByte = 0xAB){
+						continue;
+					}
+					else{
+						return 0;
+					}
+				}
+				packetByte = PacketInterface::getByte();
+				if(packetByte != 0xFC){
+					if(packetByte = 0xAB){
+						continue;
+					}
+					else{
+						return 0;
+					}
+				}
+				else{
+					return 1;
+				}
+			}
+		}
+		return 0;
+	}
+
+	int validateAocsComponentID(int subsystemID){
+		const int validSubsys[6] = {20, 21, 22, 23, 30, 31};
+		for(int i = 0; i < 6; i++){
+			if(subsystemID == validSubsys[i]){
+				return 1;
+			}
 		}
 		return 0;
 	}
@@ -122,11 +225,13 @@ public:
 private:
 	int protocol;
 	int tlmType;
-	int8_t secondaryHeader1;
-	int8_t secondaryHeader2;
-	int8_t checksum;
+	int subsystemID;
+	int componentID;
+	uint8_t secondaryHeader1;
+	uint8_t secondaryHeader2;
+	uint8_t checksum;
 	union payLoad {
 		float tempFVal;
-		int32_t tempIVal;
+		uint32_t tempIVal;
 	}payload;
 };
--- tlm_proc.cpp
+++ tlm_proc.cpp
@@ -1,46 +1,71 @@
 #include <iostream>
-#include <fstream>
+#include <stdint.h>
+#include <stdio.h>
+#include <vector>
+#include <queue>
+
+#include "tlm_parser.h"
 
 using namespace std;
-int main(void) {
-	streampos size;
-	char * memblock;
 
-	ifstream file("test.txt", ios::in | ios::binary | ios::ate);
-	if (file.is_open())
-	{
-		size = file.tellg();
-		memblock = new char[size];
-		file.seekg(0, ios::beg);
-		file.read(memblock, size);
-		file.close();
+vector<uint8_t> packetValid{0xAB, 0xBA, 0xCF, 0xFC, 0x25, 0xA1, 0xFF, 0x43, 0x83, 0xC0, 0x00, 0x7B};
+vector<uint8_t> packetSpurious1{0xAB, 0xAB, 0xAB, 0xBA, 0xCF, 0xFC, 0x25, 0xA1, 0xFF, 0x43, 0x83, 0xC0, 0x00, 0x7B};
+vector<uint8_t> packetSpurious2{0xAB, 0xAB, 0xAB, 0xBA, 0xCF, 0xFC, 0x25, 0xA1, 0xFF, 0x43, 0x83, 0xC0, 0x00, 0x7B, 0xAA, 0xBB};
+vector<uint8_t> packetMagicInvalid{0xAB, 0xCD, 0xCF, 0xFC, 0x25, 0xA1, 0xFF, 0x43, 0x83, 0xC0, 0x00, 0x7B};
+vector<uint8_t> packetChecksumInvalid{0xAB, 0xBA, 0xCF, 0xFC, 0x25, 0xA1, 0xFF, 0x43, 0x83, 0xC0, 0x00, 0xBB};
+vector<uint8_t> packetProtocolInvalid{0xAB, 0xBA, 0xCF, 0xFC, 0xA5, 0xA1, 0xFF, 0x43, 0x83, 0xC0, 0x00, 0xFB};	//protocol 5
+vector<uint8_t> packetSubsystemInvalid{0xAB, 0xBA, 0xCF, 0xFC, 0x2A, 0xA1, 0xFF, 0x43, 0x83, 0xC0, 0x00, 0x74};	//subsystem 10
+vector<uint8_t> packetComponentInvalid{0xAB, 0xBA, 0xCF, 0xFC, 0x25, 0xF1, 0xFF, 0x43, 0x83, 0xC0, 0x00, 0x2B};	//subsys 5, compnent 30
 
-		cout << "the entire file content is in memory";
-
-		delete[] memblock;
+int setPacketData(vector<uint8_t> &data, queue<uint8_t> &dataQ){
+	for(int i = 0; i < data.size(); i++){
+		dataQ.push(data[i]);
 	}
-	else cout << "Unable to open file";
 	return 0;
-	/*
-	while (1) {
-		while (iface.existRcvData()) {
+}
+
+int unit_test(vector<uint8_t>&testPacket, int detect, uint8_t subsys, uint8_t compo_id, float temperature) {
+	PacketInterface iface;
+	setPacketData(testPacket, packetDataQ);
+	Parser parser;
+
+	//cout << "Now start to parse the packet" << endl;
+	CompoTlm_t tlm = {};
+	int isDetect = 1;
+
+	while (iface.existRcvData()) {
 		uint8_t byte = iface.getByte();
 		// Parse incoming bytes and detect packet
 		if (parser.detectPkt(byte)) {
-		// One, correct packet has been detected
-		CompoTlm_t tlm = {};
-		int err = parser.extractData(tlm);
-		if (err == 0) {
-		// Pass telemetry info to the upper layer
-		system.tlm.update(tlm);
+			// One, correct packet has been detected
+			//cout << "Packet detected. Now extract!!!" << endl;
+			int err = parser.extractData(tlm);
+			if (err == 0) {
+			//printf("Extract result: subsys=%d, comp=%d, temp=%.1f\n", tlm.subsys_id, tlm.compo_id, tlm.temperature);
+			}
+			else {
+				// Error handling, etc.
+			}
 		}
-		else {
-		// Error handling, etc.
+		else{
+			isDetect = 0;
 		}
-		}
-		}
-		
-		system.time.sleep_ms(100);
 	}
-	*/
+	if(detect == isDetect && subsys == tlm.subsys_id && compo_id == tlm.compo_id && temperature == tlm.temperature){
+		return 1;
+	}
+	return 0;
+}
+
+int main(void)
+{
+	printf("Test case 1 is :%s\n", (unit_test(packetValid, 1, 5, 20, 263.5)) ? "Pass" : "False");
+	printf("Test case 2 is :%s\n", (unit_test(packetSpurious1, 1, 5, 20, 263.5)) ? "Pass" : "False");
+	printf("Test case 3 is :%s\n", (unit_test(packetSpurious2, 1, 5, 20, 263.5)) ? "Pass" : "False");
+	printf("Test case 4 is :%s\n", (unit_test(packetMagicInvalid, 0, 0, 0, 0)) ? "Pass" : "False");
+	printf("Test case 5 is :%s\n", (unit_test(packetChecksumInvalid, 0, 0, 0, 0)) ? "Pass" : "False");
+	printf("Test case 6 is :%s\n", (unit_test(packetProtocolInvalid, 1, 0, 0, 0)) ? "Pass" : "False");
+	printf("Test case 7 is :%s\n", (unit_test(packetSubsystemInvalid, 1, 0, 0, 0)) ? "Pass" : "False");
+	printf("Test case 8 is :%s\n", (unit_test(packetComponentInvalid, 1, 0, 0, 0)) ? "Pass" : "False");
+	return 0;
 }
